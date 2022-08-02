312fcad063470b174df45de503dd147a9fed1f3b
bsharpnoob <bsharpnoob@users.noreply.github.com>
2022-08-02 17:35:41 +1000

    fix: fix deploy

--- app.js
+++ app.js
@@ -137,6 +137,6 @@ app.post("/findUsers", verifyJWT, getUser , async (req,res) => {
     }
 })
 
-app.listen( process.env.PORT || PORT,() => {
-    console.log("Server started on port " + PORT)
+app.listen(process.env.PORT || 4000,() => {
+    console.log("Server started on port " + 4000);
 })
